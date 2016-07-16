81c874599408c3be4c65d2e745dc2772a8321178
Christopher N. Hesse <raymanfx@gmail.com>
2016-07-16 20:11:50 +0200

    oneplus2: Stop building libinit_msm
    Create our own lib instead.
    
    Change-Id: I6aa0941e67439e246b56681829f6d0b4551b40a2

--- init/init_oneplus2.cpp
+++ init/init_oneplus2.cpp
@@ -34,19 +34,13 @@
 #include "log.h"
 #include "util.h"
 
-#include "init_msm.h"
-
-void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *board_type) {
+void init_variant_properties() {
     char device[PROP_VALUE_MAX];
     char rf_version[PROP_VALUE_MAX];
     int rc;
 
-    UNUSED(msm_id);
-    UNUSED(msm_ver);
-    UNUSED(board_type);
-
     rc = property_get("ro.aicp.device", device);
-    if (!rc || !ISMATCH(device, "oneplus2"))
+    if (!rc || strncmp(device, "oneplus2", 8))
         return;
 
     property_get("ro.boot.rf_v1", rf_version);
@@ -69,5 +63,9 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("telephony.lteOnCdmaDevice", "1");
         property_set("ro.telephony.default_network", "9,9");
     }
+}
+
+void vendor_load_properties() {
+    init_variant_properties();
 }
 
